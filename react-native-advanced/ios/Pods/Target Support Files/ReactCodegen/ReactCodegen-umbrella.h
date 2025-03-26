50987c083bbcbeca4cd32e70e0714cbfb1407861
Mitchell <mitchiemt11@gmail.com>
2025-03-26 09:19:31 +0200

    Dependencies: React Navigation

--- react-native-advanced/ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
+++ react-native-advanced/ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
@@ -12,6 +12,22 @@
 
 #import "RCTModulesConformingToProtocolsProvider.h"
 #import "RCTThirdPartyComponentsProvider.h"
+#import "react/renderer/components/rnscreens/ComponentDescriptors.h"
+#import "react/renderer/components/rnscreens/EventEmitters.h"
+#import "react/renderer/components/rnscreens/Props.h"
+#import "react/renderer/components/rnscreens/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rnscreens/ShadowNodes.h"
+#import "react/renderer/components/rnscreens/States.h"
+#import "react/renderer/components/safeareacontext/ComponentDescriptors.h"
+#import "react/renderer/components/safeareacontext/EventEmitters.h"
+#import "react/renderer/components/safeareacontext/Props.h"
+#import "react/renderer/components/safeareacontext/RCTComponentViewHelpers.h"
+#import "react/renderer/components/safeareacontext/ShadowNodes.h"
+#import "react/renderer/components/safeareacontext/States.h"
+#import "rnscreens/rnscreens.h"
+#import "rnscreensJSI.h"
+#import "safeareacontext/safeareacontext.h"
+#import "safeareacontextJSI.h"
 
 FOUNDATION_EXPORT double ReactCodegenVersionNumber;
 FOUNDATION_EXPORT const unsigned char ReactCodegenVersionString[];
